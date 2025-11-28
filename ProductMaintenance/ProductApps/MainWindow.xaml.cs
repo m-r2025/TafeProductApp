265aba26d80f9b2cd315aa9d8fd4fe400c3773da
001119342 <Mohsen.razeh@student.tafesa.edu.au>
2025-11-29 10:13:09 +1030

    Commit the branch feat_wrap1.1

--- ProductMaintenance/ProductApps/MainWindow.xaml.cs
+++ ProductMaintenance/ProductApps/MainWindow.xaml.cs
@@ -35,6 +35,7 @@ namespace ProductApps
                 cProduct.calTotalPayment();
                 totalPaymentTextBlock.Text = Convert.ToString(cProduct.TotalPayment);
                 totalChargeTextBox.Text = Convert.ToString(cProduct.TotalPayment+25);
+                totalChargeAfterWrapTextBox.Text = Convert.ToString(cProduct.TotalPayment + 25+5);
             }
             catch (FormatException)
             {
